d9ce3a5d7d221a61eaeba4289e53f24310905c20
thiennhanng191 <ntnguyen@vassar.edu>
2022-01-07 12:46:00 -0500

    init parse config

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = appId // <- UPDATE
+                $0.clientKey = clientKey // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,9 @@
+target 'Parstagram' do
+  	# Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+	use_frameworks!
+
+	# Add Parse pods and install
+	pod 'Parse'
+	pod 'AlamofireImage'
+
+end
